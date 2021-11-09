a7ebd7372b78518ab2956ad946f4b950db343662
Nadia Wartiningrum <65618373+nadiawaa@users.noreply.github.com>
2021-11-09 20:36:47 +0700

    Add files via upload

---
+++ Arduino_IDE/MASTERUNO.ino
@@ -0,0 +1,123 @@
+#include <SoftwareSerial.h>
+#include <OneWire.h> //adc
+
+
+#define TD A2//noteini haruse pin analog
+#define SensorPin A0            //pH meter Analog output to Arduino Analog Input 0
+#define Offset 0.00            //deviation compensate
+#define samplingInterval 20
+#define printInterval 800
+#define ArrayLenth  40    //times of collection
+
+
+
+SoftwareSerial uno(2,3);
+
+
+
+int pHArray[ArrayLenth];   //Store the average value of the sensor feedback
+int pHArrayIndex=0;
+
+static float kekeruhan;
+static float teg;
+
+void setup() {
+  
+uno.begin(9600);
+Serial.begin(9600);
+
+}
+
+void loop() {
+  
+//turbidity______________________________________________________________________________
+
+  int val = analogRead(A2);
+  teg = val*(5.0/1024);
+  kekeruhan = 100.00-(teg/4.33)*100.00;
+  Serial.print(teg);
+    Serial.print("       ");
+  Serial.print("nilai adc= ");
+  Serial.print(val);
+     Serial.print("   ");
+  Serial.print("kekeruhan= ");
+  Serial.print(kekeruhan);
+  Serial.println("     NTU");
+  delay(500);
+  
+
+//ph_______________________________________________________________________________________________________
+
+  static unsigned long samplingTime = millis();
+  static unsigned long printTime = millis();
+  static float pHValue,voltage;
+  if(millis()-samplingTime > samplingInterval)
+  {
+      pHArray[pHArrayIndex++]=analogRead(SensorPin);
+      if(pHArrayIndex==ArrayLenth)pHArrayIndex=0;
+      voltage = avergearray(pHArray, ArrayLenth)*5/1024;
+      pHValue = 3.5*voltage+Offset;
+      samplingTime=millis();
+  }
+  if(millis() - printTime > printInterval)   //Every 800 milliseconds, print a numerical, convert the state of the LED indicator
+  {
+
+    Serial.print("Voltage:");
+        Serial.print(voltage,2);
+        Serial.print("    pH value: ");
+    Serial.println(pHValue,3);
+        printTime=millis();
+  }
+
+if(uno.available()>0)
+{
+  uno.write(pHValue); //mengirim data ke nodemcu
+  uno.write(kekeruhan);
+}
+}
+
+
+
+
+
+
+
+double avergearray(int* arr, int number){
+  int i;
+  int max,min;
+  double avg;
+  long amount=0;
+  if(number<=0){
+    Serial.println("Error number for the array to avraging!/n");
+    return 0;
+  }
+  if(number<5){   //less than 5, calculated directly statistics
+    for(i=0;i<number;i++){
+      amount+=arr[i];
+    }
+    avg = amount/number;
+    return avg;
+  }else{
+    if(arr[0]<arr[1]){
+      min = arr[0];max=arr[1];
+    }
+    else{
+      min=arr[1];max=arr[0];
+    }
+    for(i=2;i<number;i++){
+      if(arr[i]<min){
+        amount+=min;        //arr<min
+        min=arr[i];
+      }else {
+        if(arr[i]>max){
+          amount+=max;    //arr>max
+          max=arr[i];
+        }else{
+          amount+=arr[i]; //min<=arr<=max
+        }
+      }//if
+    }//for
+    avg = (double)amount/(number-2);
+  }//if
+  return avg;
+}
---
+++ Arduino_IDE/SUHU1.ino
@@ -0,0 +1,82 @@
+#include <ESP8266WiFi.h> 
+#include <FirebaseArduino.h>
+
+#include <OneWire.h> //adc
+#include <DallasTemperature.h> //DS18B20 sensor suhu
+#include <NewPing.h> //SR04T sensor ultrasonic
+
+#include <SoftwareSerial.h>
+SoftwareSerial mcu(D1,D2);
+
+
+
+#define FIREBASE_HOST "my-i-pond-default-rtdb.asia-southeast1.firebasedatabase.app"
+#define FIREBASE_AUTH "VqaLWbyEY9nEly5jyO3NZjAotLkAXHMfmRzXN0b5"
+#define WIFI_SSID "Abhyasa"
+#define WIFI_PASSWORD "DRA012108"
+/*
+ # This sample code is used to test the pH meter V1.0.
+ # Editor : YouYou
+ # Ver    : 1.0
+ # Product: analog pH meter
+ # SKU    : SEN0161
+*/
+
+#define ONE_WIRE_BUS D4//2 //Suhu D2
+
+OneWire oneWire(ONE_WIRE_BUS); // Setup a oneWire instance to communicate with any OneWire device    
+DallasTemperature sensors(&oneWire); // Pass oneWire reference to DallasTemperature library
+
+int pHValue;
+int kekeruhan;
+void setup(void)
+{
+  Serial.begin(9600);
+  mcu.begin(9600);
+  // connect to wifi.
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+    }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+ 
+  
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+
+}
+
+void loop(void)
+{
+
+
+
+//Temperature
+  sensors.requestTemperatures();   
+    Serial.print("Temperature  : ");  
+    Serial.print(sensors.getTempCByIndex(0)); 
+    Serial.println(" C"); 
+    Firebase.setFloat("titik1/suhu", sensors.getTempCByIndex(0));
+  delay(50); 
+
+mcu.write("mcu");
+if (mcu.available()>0)
+{
+  
+
+//ph
+  pHValue=mcu.read();
+    Serial.print("pH value: ");
+    Serial.println(pHValue);
+    Firebase.setFloat("titik1/ph", pHValue);
+
+//turbidity
+  kekeruhan=mcu.read();
+    Serial.print("turbidity: ");
+    Serial.println(kekeruhan);
+    Firebase.setFloat("titik1/turbidity", kekeruhan);
+}
+}
